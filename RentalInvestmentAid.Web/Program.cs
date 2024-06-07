3b3bb23802ae7e4f093a3a4e9e815391a953ca19
BriceDussart <bdussart@gmail.com>
2024-06-07 09:47:19 +0200

    Update cascind value and Dependency Injection

--- RentalInvestmentAid.Web/Program.cs
+++ RentalInvestmentAid.Web/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Components;
+using RentalInvestmentAid.Caching;
+using RentalInvestmentAid.Database;
 using RentalInvestmentAid.Web.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,7 +9,26 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddBlazorBootstrap();
+
+IDatabaseFactory databaseFactory = new SqlServerDatabase();
+builder.Services.AddCascadingValue(sp =>
+{
+    CachingManager cachingManager = new CachingManager(databaseFactory);
+    var source = new CascadingValueSource<CachingManager>("CachingManager", cachingManager, false);
+
+    
+
+    return source;
+});
+
+builder.Services.AddCascadingValue(sp =>
+{
+    var source = new CascadingValueSource<IDatabaseFactory>("DatabaseFactory", databaseFactory, false);
+    return source;
+});
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
